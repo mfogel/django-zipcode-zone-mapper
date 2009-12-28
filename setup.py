155caedf0a33f1450b28e4f272d7ae881c01a07b
Mike Fogel <mike@fogel.ca>
2009-12-28 23:36:32 +0000

    Reorganizing to be more like a python package.

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from distutils.core import setup
+
+setup(  name='zone-mapper',
+        version='0.1.0',
+        description='Build kml files with different colored zones',
+        author='Mike Fogel',
+        author_email='mike@fogel.ca',
+        url='http://svn.bullseyelabs.com/django-apps/zone-mapper/',
+        packages=['zone_mapper'],
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Utilities'],
+        ) 
